989064eab64a3d78fae90aafee9fefa3f9746bbe
Nathan Galindo <nathan.zebedee@gmail.com>
2023-03-14 23:14:45 -0500

    changed meta data

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -8,8 +8,8 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Nathan Galindo',
+    email: 'nathan_galindo1@baylor.edu',
   }
 
   let feed = new Feed({
